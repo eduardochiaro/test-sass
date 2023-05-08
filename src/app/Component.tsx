39b328de5ea09088accf08b9ce6581cd2ff4ceb1
Eduardo Chiaro <eduardo.chiaro@verizonwireless.com>
2023-05-08 08:48:15 -0700

    add test pages

---
+++ src/app/Component.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+export default function Component() {
+	return (
+		<p className="text-yellow-500">Test Component</p>
+	)
+}
---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import '../app/globals.scss'
+import Component from '../app/Component';
+
+export default function test () {
+  return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			Test page
+			<Component/>
+		</main>
+	)
+}
