112bb1b7a28e58e5a4b303abbf74088e63ae7978
Eduardo Chiaro <eduardo.chiaro@verizonwireless.com>
2023-05-08 08:56:33 -0700

    update text

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -4,7 +4,7 @@ import Component from '../app/Component';
 export default function test () {
   return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Test page
+			This page use `pages` folder
 			<Component/>
 		</main>
 	)
